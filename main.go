63877a3e50aa8d466bfec46113fdb87f4a74804c
Данни Титуанья <davila.tituanya@gazprombank.ru>
2026-02-12 16:42:07 +0300

    Add data model and example

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
+// Notes API - Step 1
 package main
 
 import "fmt"
 
+type Note struct {
+	ID 		int
+	Title 	string
+	Content string
+}
+
+var store = map[string]Note{}
+
+
 func main() {
+	store["1"] = Note{ ID: 1, Title: "First Note", Content: "Just a note"}
 	fmt.Println("Notes API")
 }
